5b06c8b0aad58bbf92d2d31068ba1699c13d9566
pnlybubbles <pnlybubbles@gmail.com>
2016-10-29 15:59:55 +0900

    feat: add form to index

--- index/index.js
+++ index/index.js
@@ -7,3 +7,14 @@ function rot() {
 }
 
 rot();
+
+document.getElementById('color').addEventListener(function(e) {
+  var color = e.target.value;
+  var elm = document.createElement('div');
+  elm.style.color = color;
+  if (elm.style.color === color) {
+    gr('#canvas')('#sprite').attr('color', color);
+  } else {
+    console.error(color + ' is not a real color. well done...');
+  }
+});
