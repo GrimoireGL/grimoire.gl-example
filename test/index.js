7fdf2b745f1fb297bd488e79de680306260f0e56
case2912 <shiroyama@maia.eonet.ne.jp>
2016-11-04 21:37:03 +0900

    add: t01-01

--- test/index.js
+++ test/index.js
@@ -1,5 +1,22 @@
-gr(function(){
-  var $$ = gr("#main");
-  var a = $$("mesh").get();
-  console.log(a);
+gr(function() {
+    var $$ = gr("#main");
+    // var a = $$("mesh").get();
+    // console.log(a);
+    debugger;
+    gr.registerComponent("Print", {
+        attributes: {
+            test: {
+                defaultValue: "HELLO WORLD!",
+                converter: "String"
+            }
+        },
+        $awake:()=>{
+          console.log(111);
+        }
+    });
+   $$("mesh").addComponent("Print");
+   var a = $$("mesh")("Print").getAttribute("test");
+
+    console.log(gr.componentDeclarations);
+    console.log(a);
 });
