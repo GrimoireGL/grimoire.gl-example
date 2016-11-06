46068b5d079fd68d5ebbae7ad2cafd588aff4afa
pnlybubbles <pnlybubbles@gmail.com>
2016-11-06 15:56:15 +0900

    feat: edit 10

--- t07-01/index.js
+++ t07-01/index.js
@@ -1,22 +1,22 @@
 gr.registerComponent('Rotate', {
-    attributes: {
-        speed: {
-          defaultValue: '1',
-          converter: 'Number',
-        },
+  attributes: {
+    speed: {
+      defaultValue: '1',
+      converter: 'Number',
     },
-    $mount: function() {
-      this.phi = 0;
-      // console.log(this.node.getAttribute('speed'));
-    },
-    $update: function() {
-      this.phi += this.node.getAttribute('speed');
-      this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + this.phi);
-    },
+  },
+  $mount: function() {
+    this.phi = 0;
+    // console.log(this.node.getAttribute('speed'));
+  },
+  $update: function() {
+    this.phi += this.getValue('speed');
+    this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + this.phi);
+  },
 });
 
 gr(function() {
-    var $$ = gr('#main');
-   $$('mesh').addComponent('Rotate');
-   $$('mesh').setAttribute('speed', 1);
+  var $$ = gr('#main');
+  $$('mesh').addComponent('Rotate');
+  $$('mesh').setAttribute('speed', 1);
 });
--- t10-01/index.js
+++ t10-01/index.js
@@ -1,11 +1,22 @@
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      defaultValue: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function() {
+    this.phi = 0;
+    // console.log(this.node.getAttribute('speed'));
+  },
+  $update: function() {
+    this.phi += this.getValue('speed');
+    this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + this.phi);
+  },
+});
+
+gr.registerNode("rotate", ["Rotate"], {}, "mesh");
+
 gr(function() {
-    var $$ = gr("#main");
-    gr.registerComponent("Print", {
-        attributes: {
-            test: {
-                defaultValue: "HELLO WORLD!",
-                converter: "String"
-            }
-        }
-    });
+  var $$ = gr('#main');
 });
