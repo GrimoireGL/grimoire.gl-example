65d7934579d8a4eb14c7ebe7cde4269d61388934
pnlybubbles <pnlybubbles@gmail.com>
2016-11-19 01:48:08 +0900

    fix: edit sample

--- simple/index.js
+++ simple/index.js
@@ -1,8 +1,9 @@
-var colors = ["red","blue","yellow","green"];
-gr(function(){
+gr(function() {
+  var colors = ['white', 'red', 'lime'];
   var i = 0;
-  setInterval(function(){
-    gr("script")("mesh").setAttribute("color",colors[i % 4]);
+  setInterval(function() {
+    gr('#canvas')('mesh').setAttribute('color', colors[i % 3]);
     i++;
-  },1000);
+  }, 1000);
 });
+
