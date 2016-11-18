743b9d69fe598e29f4b8672b746f5916494dcc4f
moajo <mimirosiasd@gmail.com>
2016-11-18 18:02:05 +0900

    add demo

---
+++ t90-01/index.js
@@ -0,0 +1,44 @@
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      defaultValue: '1',
+      converter: 'Number',
+    },
+  },
+  $mount: function () {
+    this.phi = 0;
+  },
+  $update: function () {
+    this.phi += this.getValue('speed');
+    this.node.setAttribute('rotation', this.phi + ',' + this.phi + ',' + this.phi);
+  },
+});
+
+gr(function () {
+  var MAIN = gr('#main');
+  MAIN('mesh').addComponent('Rotate');
+  MAIN('mesh')('Rotate').setAttribute('speed', 1);
+});
+
+
+
+$(function () {
+  $("input").change(function (v) {
+    gr("#main")("mesh").setAttribute("color", v.target.value);
+  });
+});
+
+/*
+gomlでかんたんに機能を追加できる。
+js側のインタフェース
+
+（componentをかんたんに作成してregisterできる。）
+
+goml
+mouseCameraControll
+input
+effect
+
+
+rotationComponentをregister
+ */
