566556f2d2cf9edb433ea52553d99af032324e8e
case2912 <shiroyama@maia.eonet.ne.jp>
2016-12-14 13:38:02 +0900

    add check script

--- check-example.sh
+++ check-example.sh
@@ -3,3 +3,8 @@ for i in $(seq 1 10);do
   i=`printf %03d $i`
   open http://localhost:8080/\#example-$i
 done
+
+# for a  in ./t* ; do
+# echo $a | sed -e "s/\.\///"
+# open http://localhost:8080/\#$a
+# done
